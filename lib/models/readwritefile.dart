b4c11bf72e8f23b3fb05e2ebe6518659673af732
makeItHapp <36176811+makeItHapp@users.noreply.github.com>
2019-07-12 21:24:11 -0700

    Update readwritefile.dart

--- lib/models/readwritefile.dart
+++ lib/models/readwritefile.dart
@@ -1,4 +1,15 @@
 Class PlanningFormModel{
+  String Company;
+  String Department;
+  List<String> costElements;
+  
+  PlanningFormModel(){
+  }
+  
+  String toString(){
+    String planningFormInString = ""+this.Company+" "+this.Department;
+    return planningFormInString;
+    
 }
 
 class ReadFIle{
