5ffcbb6d13d6acf8f3c56d50f11cb638d3a0444c
PrakashBidari <pbidari46@gmail.com>
2019-07-14 12:06:06 +0545

    change2

--- lib/models/readwritefile.dart
+++ lib/models/readwritefile.dart
@@ -1,20 +1,16 @@
-Class PlanningFormModel{
+class PlanningFormModel {
   String Company;
   String Department;
   List<String> costElements;
-  
-  PlanningFormModel(){
-  }
-  
-  String toString(){
-    String planningFormInString = ""+this.Company+" "+this.Department;
+
+  PlanningFormModel() {}
+
+  String toString() {
+    String planningFormInString = "" + this.Company + " " + this.Department;
     return planningFormInString;
-    
+  }
 }
 
-class ReadFIle{
+class ReadFIle {}
 
-}
-class WriteFile{
-  
-}
+class WriteFile {}
