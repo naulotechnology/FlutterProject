2e0d3f8f0cbc7a198e3711acd368a8aeb68df424
Khanal <ram.khanal@accenture.com>
2019-07-14 02:40:51 -0700

    updates to Planning Form Model

--- lib/models/readwritefile.dart
+++ lib/models/readwritefile.dart
@@ -1,79 +1,70 @@
 
 
-void main() {
-  
-  PlanningFormModel pfm = new PlanningFormModel();  
-  
-  pfm.Company = "N Tech";
-  pfm.Department = "Marketing";
-  pfm.costElements = new List<String>();
-  
-  pfm.costElements.add("Transportation");
-  pfm.costElements.add("Marketing");
-  pfm.costElements.add("Human Resources");
-  pfm.costElements.add("Information Technology");
-  pfm.costElements.add("Legal");
-  
-  //instatiate the map to store monthly plan for each costEleemnts
-  Map mp = new Map<String,MonthlyPlan>();
+  class PlanningFormModel{
+    String Company;
+    String Department;
+    List<String> costElements;
+    Map<String, MonthlyPlan> monthLevelPlan;
 
-  
-  for(String ce in pfm.costElements){
-    //creaete monthly plan for each cost element 
-    MonthlyPlan mPlan = new MonthlyPlan();
-    mPlan.category = ce;
-    List amt = new List<int>();
     
-    for (int i = 1; i < 13; i++) {
-      //assign some amount to each of the 12 months
-      amt.add(i*125+.25);
+    PlanningFormModel(){
+      this.Company = "N Tech";
+      this.Department = "Marketing";
+      this.costElements = new List<String>();
+    
+      this.costElements.add("Transportation");
+      this.costElements.add("Marketing");
+      this.costElements.add("Human Resources");
+      this.costElements.add("Information Technology");
+      this.costElements.add("Legal");
+    
+      //instatiate the map to store monthly plan for each costEleemnts
+      Map mp = new Map<String,MonthlyPlan>();
+
+      for(String ce in this.costElements){
+        //creaete monthly plan for each cost element 
+        MonthlyPlan mPlan = new MonthlyPlan();
+        mPlan.category = ce;
+        List amt = new List<int>();
+      
+        for (int i = 1; i < 13; i++) {
+          //assign some amount to each of the 12 months
+          amt.add(i*125+.25);
+        }
+        mPlan.amountInMonth = amt;
+      
+        List hr = new List<int>();
+      
+        for (int i = 1; i < 13; i++) {
+          //assign some amount to each of the 12 months
+          hr.add(i*7);
+        }
+        mPlan.hourInMonth = hr;
+
+        //add monthly plan for the is 
+        mp[ce] = mPlan;
+      }
+      //assign month plan
+      this.monthLevelPlan = mp;
+      
     }
-    mPlan.amountInMonth = amt;
     
-    List hr = new List<int>();
-    
-    for (int i = 1; i < 13; i++) {
-      //assign some amount to each of the 12 months
-      hr.add(i*7);
+    String toString(){
+      String planningFormInString = ""+this.Company+" "+this.Department;
+      return planningFormInString;
     }
-    mPlan.hourInMonth = hr;
-
-    //add monthly plan for the is 
-    mp[ce] = mPlan;
+      
   }
-  
-  //assign month plan
-  pfm.monthLevelPlan = mp;
-  
- 
-}
 
-class PlanningFormModel{
-  String Company;
-  String Department;
-  List<String> costElements;
-  Map<String, MonthlyPlan> monthLevelPlan;
+  class MonthlyPlan{
+    String category;
+    List<int> amountInMonth;
+    List<int> hourInMonth;
+  }
 
-  
-  PlanningFormModel(){
+  class ReadFIle{
+
   }
-  
-  String toString(){
-    String planningFormInString = ""+this.Company+" "+this.Department;
-    return planningFormInString;
-  }
+  class WriteFile{
     
-}
-
-class MonthlyPlan{
-  String category;
-  List<int> amountInMonth;
-  List<int> hourInMonth;
-}
-
-class ReadFIle{
-
-}
-class WriteFile{
-  
-}
+  }
