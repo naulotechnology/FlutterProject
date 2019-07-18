3a020949cc5bcca546c2600a5e195967ee7f075b
Khanal <ram.khanal@accenture.com>
2019-07-17 21:16:10 -0700

    syncing Asish's code in myAdminForm

--- lib/screen/myadminform.dart
+++ lib/screen/myadminform.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterproject/models/readwritefile.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
+
 
 class MyAdminForm extends StatefulWidget {
   @override
@@ -12,14 +17,64 @@ class MyAdminForm extends StatefulWidget {
 class AdminForm extends State {
   Storage st;
   @override
-  void initState() {
-    st = new Storage();
-    super.initState();
-  }
+  // void initState() {
+  //   st = new Storage();        chgd b m
+  //   super.initState();
+  // }
 
   var displayResult="";
   TextEditingController companyController = TextEditingController();
   TextEditingController departmentController = TextEditingController();
+
+  File jsonFile;
+  Directory dir;
+  String fileName = "myJSONFile.json";
+  bool fileExists = false;
+  Map<String, String> fileContent;
+
+ @override
+  void initState() {
+    st = new Storage();
+    super.initState();
+    getApplicationDocumentsDirectory().then((Directory directory) {
+      dir = directory;
+      jsonFile = new File(dir.path + "/" + fileName);
+      fileExists = jsonFile.existsSync();
+      if (fileExists) this.setState(() => fileContent = json.decode(jsonFile.readAsStringSync()));           //
+    });
+  }
+
+
+
+
+  void createFile(Map<String, String> content, Directory dir, String fileName) {
+    print("Creating file!");
+    File file = new File(dir.path + "/" + fileName);
+    file.createSync();
+    fileExists = true;
+    file.writeAsStringSync(json.encode(content));                                                     //
+  }
+
+
+
+  void writeToFile(String key, String value) {
+    print("Writing to file!");
+    Map<String, String> content = {key: value};
+    if (fileExists) {
+      print("File exists");
+      Map<String, String> jsonFileContent = json.decode(jsonFile.readAsStringSync());
+      jsonFileContent.addAll(content);
+      jsonFile.writeAsStringSync(json.encode(jsonFileContent));                                                  //
+    } else {
+      print("File does not exist!");
+      createFile(content, dir, fileName);
+    }
+    this.setState(() => fileContent = json.decode(jsonFile.readAsStringSync()));                              //
+    print(fileContent);
+  }
+
+
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +84,21 @@ class AdminForm extends State {
       body: SafeArea(
         child: Form(
           child: ListView(children: <Widget>[
+
+            new Text(fileContent.toString()),
+
             Padding(
               padding: EdgeInsets.only(bottom: 30),
             ),
+
+             
+
             Container(child: Text("Company: ", style: textStyle)),
             Container(
+
+               
+              
+
               child: TextFormField(
                 keyboardType: TextInputType.text,
                 style: textStyle,
@@ -85,13 +150,19 @@ class AdminForm extends State {
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   RaisedButton(
+                    
                     onPressed: () {
-                      setState(() {
-                        print("Hellow Prakash");
-                        this.displayResult = names();
-                       st.writeData(displayResult);
-                        child: Text('Write to File');
-                      });
+                      writeToFile(companyController.text, departmentController.text);
+                      // setState(() {
+                      //   print("Hellow Prakash");
+                      // //   this.displayResult = names();
+                      // //  st.writeData(displayResult);
+                        
+                      
+
+
+                      //   child: Text('Write to File');
+                      // });
                     },
                     textColor: Colors.white,
                     padding: const EdgeInsets.all(0.0),
