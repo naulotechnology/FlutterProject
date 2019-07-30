8e4523eb84eb76c02a077aa7f7b6ad9de776504d
aacesh <aaceshm@gmail.com>
2019-07-30 13:26:44 +0545

    commit

--- lib/models/readwritefile.dart
+++ lib/models/readwritefile.dart
@@ -7,20 +7,19 @@ class PlanningFormModel {
   String Department;
   List<String> costElements;
   Map<String, MonthlyPlan> monthLevelPlan;
+  MonthlyPlan mPlan = new MonthlyPlan();
   String currentSavedState;
   String cost = "";
   String savedStateFromFile="This is default";
+  Storage st ;
 
-  //String get Company / Aa
-  //String get Department /Aa
-  //List<String> get CostElement/ Aa
-  //Map  monthlyLevelPlan / Aa
-  //implements to toString()
-  //implement to Json
+
+  
 
   PlanningFormModel() {
     this.Company = "N Tech";
     this.Department = "Marketing";
+    this.st = new Storage();
     this.costElements = new List<String>();
 
     this.costElements.add("Transportation");
@@ -91,12 +90,59 @@ class PlanningFormModel {
       this.monthLevelPlan[costElement].amountInMonth;
     }
   }
+
+ savePfmToFile(){
+   this.st.writeData(this.PlanningFormModeltoJsonv2());
+ }
+   String PlanningFormModeltoJsonv2() {
+    String p = "";
+    p = p + "{";
+    p = p + "'Company':" + "'${this.Company}'" + ",";
+    p = p + "'Department':" + "'${this.Department}'" + ",";
+     p = p + "'costElements':[";
+    for(String ce in this.costElements){
+      p = p + "'" + ce + "'," ;
+    }
+     p = p +"],";
+    p = p + "{'ceToMpMap':[" +  "${mPlan.monthlyplantoJsonv2()}"+"]";
+   
+  
+    p = p + "}";
+    p = json.encode(p);
+    return p;
+  }
+
 }
+
 
 class MonthlyPlan {
   String category;
   List<int> amountInMonth;
   List<int> hourInMonth;
+
+    String monthlyplantoJsonv2() {
+    PlanningFormModel pfm = new PlanningFormModel();
+    String s = "";
+    for (String ce in pfm.costElements) {
+      s = s + "{";
+      s = s + "'" + ce + "':{";
+      s = s + "'amountInMonth':[";
+      for (int i in amountInMonth) {
+        s = s + "'" + i.toString() + "',";
+      }
+      s = s + "],";
+
+      s = s + "'hourInMonth':[";
+      for (int i in hourInMonth) {
+        s = s + "'" + i.toString() + "',";
+      }
+      s = s + "]}},";
+
+      // s = json.encode(s);
+    }
+
+    return s;
+  }
 
   List<int> getMonthlyPlan(bool isHour) {
     if (isHour)
@@ -132,4 +178,5 @@ class Storage {
     final file = await localFile;
     return file.writeAsString("$data");
   }
+
 }
