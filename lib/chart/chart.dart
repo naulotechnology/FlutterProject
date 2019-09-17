396231c04dc857acdff5dbe804e87624fc665700
PrakashBidari <pbidari46@gmail.com>
2019-09-17 11:32:26 +0545

    commit

--- lib/chart/chart.dart
+++ lib/chart/chart.dart
@@ -2,43 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 
-class MyChart extends StatefulWidget{
+class MyChart extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return ActualPlanVarianceChart();
   }
-
 }
 
-class ChartPerYear{
-final String year;
+class ChartPerYear {
+  final String year;
   final int clicks;
   final charts.Color color;
 
-  ChartPerYear(this.year,this.clicks,Color color)
-    : this.color = new charts.Color(r: color.red,g: color.green,b: color.blue,a: color.alpha);
-
+  ChartPerYear(this.year, this.clicks, Color color)
+      : this.color = new charts.Color(
+            r: color.red, g: color.green, b: color.blue, a: color.alpha);
 }
 
-class ActualPlanVarianceChart extends State{
+class ActualPlanVarianceChart extends State {
   int _counter = 0;
 
-  void _incrementCounter(){
+  void _incrementCounter() {
     setState(() {
-     _counter++; 
+      _counter++;
     });
   }
-    @override
+
+  @override
   Widget build(BuildContext context) {
     var data = [
-       new ChartPerYear('jan', 12, Colors.red),
-      new ChartPerYear('fab', 42, Colors.yellow),
-      new ChartPerYear('mar', 20, Colors.green),
-      new ChartPerYear('apr', 60, Colors.black),
-      new ChartPerYear('may', _counter+10, Colors.pink),
+      new ChartPerYear('jan', 1200, Colors.red),
+      new ChartPerYear('fab', 4200, Colors.yellow),
+      new ChartPerYear('mar', 3000, Colors.green),
+      new ChartPerYear('apr', 6000, Colors.black),
+      new ChartPerYear('may', 2500, Colors.pink),
+      new ChartPerYear('jun', 3500, Colors.pink),
+      new ChartPerYear('jul', 1200, Colors.red),
+      new ChartPerYear('aug', 4200, Colors.yellow),
+      new ChartPerYear('sep', 3000, Colors.green),
+      new ChartPerYear('oct', 6000, Colors.black),
+      new ChartPerYear('nov', 2500, Colors.pink),
+      new ChartPerYear('dec', 3500, Colors.pink),
     ];
 
-     var series = [
+    var series = [
       new charts.Series(
         domainFn: (ChartPerYear clickData, _) => clickData.year,
         measureFn: (ChartPerYear clickData, _) => clickData.clicks,
@@ -53,7 +60,7 @@ class ActualPlanVarianceChart extends State{
       animate: true,
     );
     var chartWidget = new Padding(
-      padding: new EdgeInsets.all(32.0),
+      padding: new EdgeInsets.only(left: 25),
       child: new SizedBox(
         height: 200.0,
         child: chart,
@@ -83,5 +90,4 @@ class ActualPlanVarianceChart extends State{
       ),
     );
   }
-
 }
