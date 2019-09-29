582f5385b0ebeee0a8785377da4ba8437e98a321
PrakashBidari <pbidari46@gmail.com>
2019-09-29 17:29:55 +0545

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutterproject/models/readwritefile.dart';
+import 'package:flutterproject/screen/Setting.dart';
+import 'package:flutterproject/screen/checking.dart';
 import 'package:flutterproject/screen/planninglayout.dart';
-import 'package:flutterproject/screen/satting.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 import 'chart/chart.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(), 
+      home: HomePage(),
     );
   }
 }
@@ -29,7 +30,8 @@ class HomePage extends StatefulWidget {
   final drawerItems = [
     new DrawerItem("MyTable", Icons.table_chart),
     new DrawerItem("MyForm", Icons.folder),
-    new DrawerItem("Chart", Icons.settings),
+    new DrawerItem("Chart", Icons.chat),
+    new DrawerItem("CheckData", Icons.check),
     new DrawerItem("Setting", Icons.settings)
   ];
 
@@ -48,11 +50,13 @@ class _HomepageState extends State<HomePage> with TickerProviderStateMixin {
       case 0:
         return new MyForm(pfm);
       case 1:
-        return new SavedStateFromFile(pfm);  
+        return new SavedStateFromFile(pfm);
       case 2:
         return new MyChart();
-      default:
+      case 3:
         return new SavedStateFromFile(pfm);
+      default:
+        return new Setting();
     }
   }
 
@@ -105,35 +109,81 @@ class _HomepageState extends State<HomePage> with TickerProviderStateMixin {
   }
 }
 
+// // class MySplashScreen extends StatefulWidget {
+// //   @override
+// //   _MyAppState createState() =>  _MyAppState();
+// // }
 
-// class MySplashScreen extends StatefulWidget {
-//   @override
-//   _MyAppState createState() =>  _MyAppState();
+// // class _MyAppState extends State<MySplashScreen> {
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     List<Color> color = List<Color>();
+// //     color.add(Colors.red);
+// //     color.add(Colors.blue);
+// //     color.add(Colors.green);
+
+// //     return  SplashScreen(
+// //       seconds: 4,
+// //       navigateAfterSeconds:  HomePage(),
+// //       title:  Text('Welcome,SplashScreen',
+// //       style: TextStyle(color: Colors.white,
+// //         fontWeight: FontWeight.bold,
+// //         fontSize: 20.0
+// //       ),),
+// //      // image: Image.asset('image1.jpg'),
+// //       backgroundColor: Color.fromARGB(200, 226, 66, 23),
+// //       styleTextUnderTheLoader: TextStyle(),
+// //       onClick: ()=>print("Flutter Egypt"),
+// //       loaderColor: Colors.white
+// //     );
+// //   }
+// // }
+
+// import 'package:flutter/material.dart';
+
+// void main(){
+//   runApp(new MaterialApp(
+//     home: new MyApp(),
+//   ));
 // }
 
-// class _MyAppState extends State<MySplashScreen> {
-  
-  
+// class MyApp extends StatefulWidget {
+//   @override
+//   _State createState() => new _State();
+// }
+
+// //State is information of the application that can change over time or when some actions are taken.
+// class _State extends State<MyApp>{
+
+//  bool _value1 = false;
+//  bool _value2 = false;
+
+//  void _onChanged1(bool value) => setState(() => _value1 = value);
+//  void _onChanged2(bool value) => setState(() => _value2 = value);
+
 //   @override
 //   Widget build(BuildContext context) {
-//     List<Color> color = List<Color>();
-//     color.add(Colors.red);
-//     color.add(Colors.blue);
-//     color.add(Colors.green);
-
-//     return  SplashScreen(
-//       seconds: 4,
-//       navigateAfterSeconds:  HomePage(),
-//       title:  Text('Welcome,SplashScreen',
-//       style: TextStyle(color: Colors.white,
-//         fontWeight: FontWeight.bold,
-//         fontSize: 20.0
-//       ),),
-//      // image: Image.asset('image1.jpg'),
-//       backgroundColor: Color.fromARGB(200, 226, 66, 23),
-//       styleTextUnderTheLoader: TextStyle(),
-//       onClick: ()=>print("Flutter Egypt"),
-//       loaderColor: Colors.white
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: new Text('Name here'),
+//       ),
+//       //hit Ctrl+space in intellij to know what are the options you can use in flutter widgets
+//       body: new Container(
+//         padding: new EdgeInsets.all(32.0),
+//         child: new Center(
+//           child: new Column(
+//             children: <Widget>[
+//               new Switch(value: _value1, onChanged: _onChanged1),
+//               new SwitchListTile(
+//                   value: _value2,
+//                   onChanged: _onChanged2,
+//                   title: new Text('Hello World', style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.red)),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
 //     );
 //   }
 // }
